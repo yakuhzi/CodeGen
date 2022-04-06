af82ddb5c74385d3c66f7d8d99d2b4e22da82a15
yakuhzi <miguel@heidegger-ueberlingen.de>
2022-04-06 17:28:54 +0200

    Feat: Zip files after tokenization

--- scripts/transcoder/preprocess.sh
+++ scripts/transcoder/preprocess.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --time=24:00:00
-#SBATCH --mem=10GB
-#SBATCH --partition multiple
-#SBATCH --gres=gpu:0
-#SBATCH --job-name=train_data_mlm_transcoder
-#SBATCH --output=train_data_mlm_transcoder_%j.out
 
-DATASET_PATH='dataset/Python'
-TEMP_DIR='dataset/Python/temp'
+DATASET_PATH='dataset/CPP'
+TEMP_DIR="${DATASET_PATH}/temp"
+MODE='monolingual_functions'
 NGPU=1
 
-for FILE in $DATASET_PATH/*.gz
+for FILE in "$DATASET_PATH"/*.gz
 do
-  mkdir $TEMP_DIR
-  cp $FILE $TEMP_DIR
+  mkdir "$TEMP_DIR"
+  cp "$FILE" "$TEMP_DIR"
 
   python -m codegen_sources.preprocessing.preprocess \
       --langs cpp java python \
-      --mode=monolingual_functions \
+      --mode="$MODE" \
       --local=True \
       --bpe_mode=fast \
       --train_splits="$NGPU" \
       "$TEMP_DIR"
 
-  for OUT_FILE in $TEMP_DIR/*.tok
+  for OUT_FILE in "$TEMP_DIR"/*.tok
   do
-    mv $OUT_FILE $DATASET_PATH
+    zip "$OUT_FILE".zip "$OUT_FILE"
   done
 
-  rm -r $TEMP_DIR
+  for ZIP_FILE in "$TEMP_DIR"/*.zip
+  do
+    mv "$ZIP_FILE" "$DATASET_PATH"
+  done
+
+  rm -r "$TEMP_DIR"
 done
