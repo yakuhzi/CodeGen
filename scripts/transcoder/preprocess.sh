5156eaac4ecbf2a5215dcbe72e674da0f110e187
miguel-heidegger <miguel.heidegger@sap.com>
2022-04-06 13:05:45 +0200

    Feat: Run preprocessing sequential

--- scripts/transcoder/preprocess.sh
+++ scripts/transcoder/preprocess.sh
@@ -7,13 +7,27 @@
 #SBATCH --job-name=train_data_mlm_transcoder
 #SBATCH --output=train_data_mlm_transcoder_%j.out
 
-DATASET_PATH=$(ws_find code-gen)/dataset/single
+DATASET_PATH='dataset/Python'
+TEMP_DIR='dataset/Python/temp'
 NGPU=1
 
-python -m codegen_sources.preprocessing.preprocess \
-    --langs cpp java python \
-    --mode=monolingual \
-    --local=False \
-    --bpe_mode=fast \
-    --train_splits="$NGPU" \
-    "$DATASET_PATH"
+for FILE in $DATASET_PATH/*.gz
+do
+  mkdir $TEMP_DIR
+  cp $FILE $TEMP_DIR
+
+  python -m codegen_sources.preprocessing.preprocess \
+      --langs cpp java python \
+      --mode=monolingual_functions \
+      --local=True \
+      --bpe_mode=fast \
+      --train_splits="$NGPU" \
+      "$TEMP_DIR"
+
+  for OUT_FILE in $TEMP_DIR/*.tok
+  do
+    mv $OUT_FILE $DATASET_PATH
+  done
+
+  rm -r $TEMP_DIR
+done
