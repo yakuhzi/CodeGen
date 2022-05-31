7e5164941ce035fa5e1b8a30bf63ca9afcfa8d45
yakuhzi <miguel@heidegger-ueberlingen.de>
2022-05-31 11:04:10 +0200

    Feat: Improve java parser

--- codegen_sources/scripts/parsing/parse.py
+++ codegen_sources/scripts/parsing/parse.py
@@ -1,16 +1,37 @@
 import os
 import sys
+from unittest import result
 import torch
 from tree_sitter import Language, Parser, TreeCursor
 from codegen_sources.model.translate import Translator
 
 
 DATASET_PATH = 'dataset/transcoder/test'
-MODEL_PATH='models/transcoder/TransCoder_model_1.pth'
+MODEL_PATH = 'models/transcoder/TransCoder_model_1.pth'
 BPE_PATH = 'data/bpe/cpp-java-python/codes'
 
 
-        
+def traverse_tree(cursor: TreeCursor, retracing=False):
+    nodes = []
+
+    if not retracing and cursor.node.type != "block" and cursor.goto_first_child():
+        return traverse_tree(cursor)
+
+    if cursor.node.type != "block" and cursor.goto_next_sibling():
+        return traverse_tree(cursor)
+
+    if cursor.node.type != "block" and cursor.goto_parent():
+        return traverse_tree(cursor, True)
+
+    
+    cursor.goto_first_child()
+    nodes.append(cursor.node)
+
+    while cursor.goto_next_sibling():
+        nodes.append(cursor.node)
+
+    return (n for n in nodes if n.type != "{" and n.type != "}")
+
 def parse_cpp(code: str, cursor: TreeCursor):
     print("========================================")
     assert cursor.goto_first_child()
@@ -19,32 +40,35 @@ def parse_cpp(code: str, cursor: TreeCursor):
 
 def parse_java(code: str, cursor: TreeCursor):
     print("========================================")
-    
-    assert cursor.goto_first_child()
-    #print(code[cursor.node.start_byte: cursor.node.end_byte])
-    assert cursor.goto_next_sibling()
-    #print(code[cursor.node.start_byte: cursor.node.end_byte])
-    assert cursor.goto_next_sibling()
-    #print(code[cursor.node.start_byte: cursor.node.end_byte])
-    assert cursor.goto_first_child()
-    #print(code[cursor.node.start_byte: cursor.node.end_byte])
+    input = []
+    output = []
 
-    while cursor.goto_next_sibling():
-        if sys.getsizeof(code) - cursor.node.end_byte != 34:
-            input = code[cursor.node.start_byte: cursor.node.end_byte].decode("utf-8")
-            print("INPUT", input)
-            input = f"public static int a() {{\n    {input}\n}}"
+    for node in traverse_tree(cursor):
+        snippet = code[node.start_byte: node.end_byte].decode("utf-8")
+        input.append(snippet)
 
-            with torch.no_grad():
-                output = translator.translate(
-                    input,
-                    lang1='java',
-                    lang2='python',
-                    beam_size=1,
-                )
+        with torch.no_grad():
+            result = translator.translate(
+                f"public static int a() {{\n    {snippet}\n}}",
+                lang1='java',
+                lang2='python',
+                beam_size=1,
+            )
 
-                output = '\n'.join(output[0].split("\n")[1:-2])
-                print("OUTPUT", output)
+            translation = '\n'.join(result[0].replace("\n\n", "\n").split("\n")[1:-1])
+
+            if "return" in translation.split("\n")[-1] and snippet.count("return") != translation.count("return"):
+                translation = '\n'.join(translation.split("\n")[:-1])
+
+            output.append(translation)
+
+    print("=" * 100)
+    print("INPUT")
+    print("\n".join(input))
+
+    print("=" * 100)
+    print("OUTPUT")
+    print("\n".join(output))
 
 
 def parse_python(code: str, cursor: TreeCursor):
@@ -67,11 +91,12 @@ def parse_file(path: str):
 
             if language == "cpp":
                 parse_cpp(code, cursor)
-                return
             elif language == "java":
                 parse_java(code, cursor)
             elif language == "python":
                 parse_python(code, cursor)
+            return
+
 
 if __name__ == "__main__":
     Language.build_library(
